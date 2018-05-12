0e68508f4408c62e2d6cdbcfdcf3592ee4e0abb3
Zach Snyder <sadin@fedoraproject.org>
2018-05-11 23:29:01 -0400

    deleted what I had, following a guide for 3.6

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-import threading, os, time
-from datetime import datetime, timedelta, timezone, date
+import socket
+
+# to be moved into cfg file eventually
+# Config
+server = 'chat.freenode.net'
+port = 7000
+bot_nick = 'snoid_bot'
+channel_name = '##XAMPP'
+
 
 
 def main():
     print(f'Starting...')
+
 
 
 if __name__ == '__main__':
